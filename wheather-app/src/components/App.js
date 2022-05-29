b565b0ea22cf84f967d3f214d10c99674cf9334c
Wali Ullah <waliullah0540@gmail.com>
2022-05-29 15:09:05 +0500

    wheather app added

---
+++ wheather-app/src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Wheather from "./Wheather";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Wheather />
+      </div>
+    );
+  }
+}
+
+export default App;
